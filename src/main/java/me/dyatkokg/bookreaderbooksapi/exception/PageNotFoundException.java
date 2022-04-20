0913d447fb18e7966c74867ba664d3df5bf738ce
dyatkokg <dyatkokg@gmail.com>
2022-04-20 22:58:02 +0400

    add PageNotFoundException

---
+++ src/main/java/me/dyatkokg/bookreaderbooksapi/exception/PageNotFoundException.java
@@ -0,0 +1,12 @@
+package me.dyatkokg.bookreaderbooksapi.exception;
+
+public class PageNotFoundException extends RuntimeException{
+
+    public PageNotFoundException(){
+        super("This page was not found");
+    }
+
+    public PageNotFoundException(String id) {
+        super(String.format("Page with number %s was not found", id));
+    }
+}
--- src/main/java/me/dyatkokg/bookreaderbooksapi/exception/handler/APIExceptionHandler.java
+++ src/main/java/me/dyatkokg/bookreaderbooksapi/exception/handler/APIExceptionHandler.java
@@ -3,6 +3,7 @@ package me.dyatkokg.bookreaderbooksapi.exception.handler;
 import lombok.extern.slf4j.Slf4j;
 import me.dyatkokg.bookreaderbooksapi.exception.BookNotFoundException;
 import me.dyatkokg.bookreaderbooksapi.exception.IncorrectFileTypeException;
+import me.dyatkokg.bookreaderbooksapi.exception.PageNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,5 +23,11 @@ public class APIExceptionHandler {
     public ResponseEntity<Object> handleBookNotFound(BookNotFoundException exception) {
         log.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.NO_CONTENT.ordinal()).body("Book was not found. Try again!");
+    }
+
+    @ExceptionHandler({PageNotFoundException.class})
+    public ResponseEntity<Object> handlePageNotFound(PageNotFoundException exception) {
+        log.error(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Page was not found. Try again!");
     }
 }
